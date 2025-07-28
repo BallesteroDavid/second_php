809ea5410d377e73eb0ec37652bce9974dd2cddc
BallesteroDavid <david.ballestero@outlook.fr>
2025-07-28 11:52:48 +0200

    third

--- boucle.php
+++ boucle.php
@@ -1,1 +1,8 @@
 <?php
+
+//  tableau => si il y a plusieurs éléments,  bien mettre un "s" à la fin de la variable pour signaler qu'il y a plusieurs éléments
+
+$pokemons = ["pikachu", "salameche", "arceus", "ronflex", "lucario", "tadmorv"];
+
+// boucle
+
